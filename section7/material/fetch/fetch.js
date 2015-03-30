e94f9a7dbf4b245d427686f67f09b77d5be80fbb
David Chouinard <david@davidchouinard.com>
2015-03-29 19:57:15 -0500

    Updated fetch script with final tweaks

--- section7/material/fetch/fetch.js
+++ section7/material/fetch/fetch.js
@@ -5,7 +5,7 @@
 fs = require('fs');
 request = require('request');
 
-var API_KEY = "z4djwzbmxnp66psdn4ayjmur" //TODO: delete
+var API_KEY = "" //TODO: add your API key!
 
 var file = fs.createWriteStream('../data.json');
 
@@ -22,30 +22,35 @@ function get_products_for_page(page) {
       return;
     }
 
-    console.log("Page " + data.currentPage + "/" + data.totalPages); // print our progress
+    try {
+      var data = JSON.parse(body);
 
-    var data = JSON.parse(body);
+      console.log("Page " + data.currentPage + "/" + data.totalPages); // print our progress
 
-    if (data.products.length == 0) {
-      // we've reached the end
-      file.write("]");
-      file.end();
-    } else {
-      // We could just maintain a global array that we add every page of products to. Something like this:
-      //    all_products = all_products.concat(data.products);
-      // However, this requires all data to be kept in memory until the very end. Instead, we incrementally write
-      // each page to the output file and do some string manipulations to merge the individual JSON arrays.
+      if (data.products.length == 0) {
+        // we've reached the end
+        file.write("]");
+        file.end();
+      } else {
+        // We could just maintain a global array that we add every page of products to. Something like this:
+        //    all_products = all_products.concat(data.products);
+        // However, this requires all data to be kept in memory until the very end. Instead, we incrementally write
+        // each page to the output file and do some string manipulations to merge the individual JSON arrays.
 
-      var stringified = JSON.stringify(data.products, null, '\t')
+        var stringified = JSON.stringify(data.products, null, '\t')
 
-      // Removes the start and end bracket from stringified JSON
-      //  Note that this approach will generate a trailing comma for the last element (technically invalid JSON),
-      //  but still accepted by modern browsers
-      stringified = stringified.slice(2, -2) + ',\n';
+        // Removes the start and end bracket from stringified JSON
+        //  Note that this approach will generate a trailing comma for the last element (technically invalid JSON),
+        //  but still accepted by modern browsers
+        stringified = stringified.slice(2, -2) + ',\n';
 
-      file.write(stringified);
+        file.write(stringified);
 
-      // get next page
+        // get next page
+        get_products_for_page(page + 1);
+      }
+    } catch (e) {
+      console.error(e);  // there was an error, move on to the next page
       get_products_for_page(page + 1);
     }
   })
